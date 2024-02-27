cccea181e6fbdb924cc90b1055bc0df5b9a29e96
doing-computer-stuff <118572359+doing-computer-stuff@users.noreply.github.com>
2024-02-26 20:54:02 -0600

    Add files via upload

---
+++ prototype_1/src/main/java/com/example/prototype_1/Inventory.java
@@ -0,0 +1,29 @@
+package com.example.prototype_1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Inventory {
+
+    private static Inventory newInventory;
+    private Map<String, Integer> inventory = new HashMap<String, Integer>();
+
+    private Inventory() {}                          //This makes it so
+                                                    //only one Inventory object
+    public static Inventory getInstance() {         //can be created
+        if (newInventory == null)                   //like a real database.
+            newInventory = new Inventory();
+        return newInventory;
+    }
+
+    public void addItem(String itemName, Integer quantity) {
+        this.inventory.put(itemName, quantity);
+    }
+
+    public Integer returnQuantity(String itemName) {
+        if (!this.inventory.containsKey(itemName))
+            return -1;
+        else
+            return this.inventory.get(itemName);
+    }
+}
