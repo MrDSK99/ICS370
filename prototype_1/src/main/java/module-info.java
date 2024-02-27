7d246e54121751c1a90492d17cc5d2301da4ba06
doing-computer-stuff <118572359+doing-computer-stuff@users.noreply.github.com>
2024-02-26 20:52:56 -0600

    Delete prototype_1 directory

--- prototype_1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.prototype_1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.prototype_1 to javafx.fxml;
-    exports com.example.prototype_1;
-}
