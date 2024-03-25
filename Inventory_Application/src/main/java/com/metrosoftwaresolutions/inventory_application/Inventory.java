b3f4515085f5e6f5ef4fcb11306f61ce69c3de6b
Daniel Kouassi <143004381+MrDSK99@users.noreply.github.com>
2024-03-25 16:02:43 -0500

    Update Inventory.java
    I implemented functionality to load existing inventory data from a JSON file when the application starts (Main_Application.java, Inventory.java). I also added a method to retrieve the quantity of a specific product from the JSON file (Inventory.java). Additionally, I updated the view controller to load different views (View_Controller.java). Finally, I created a JSON file to store the inventory data (inventory.json). These changes ensure that the application can read and write inventory data to and from a JSON file, providing persistence between application sessions.

--- Inventory_Application/src/main/java/com/metrosoftwaresolutions/inventory_application/Inventory.java
+++ Inventory_Application/src/main/java/com/metrosoftwaresolutions/inventory_application/Inventory.java
@@ -2,38 +2,74 @@ package com.metrosoftwaresolutions.inventory_application;
 
 import java.util.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import java.io.IOException;
+import java.util.*;
+
 public class Inventory {
 
     private static Inventory newInventory;
-    private Map<String, Integer> inventory = new HashMap<String, Integer>();
 
-    private Inventory() {}                          //This makes it so
-                                                    //only one Inventory object
-    public static Inventory getInstance() {         //can be created
-        if (newInventory == null)                   //simulating a database.
+    private List<Product> products;   // Instance variable to hold list of products
+    private ObjectMapper mapper;
+    private final String JSON_FILE = "//Inventory_Application/inventory.json";  //Path to the json file
+   
+
+   private Inventory() {
+        products = new ArrayList<>();
+        mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+       loadInventory();
+    }                        
+                                                    
+    public static Inventory getInstance() {         
+        if (newInventory == null)                   
             newInventory = new Inventory();
         return newInventory;
     }
 
     public void addItem(String itemName, Integer quantity) {
-        this.inventory.put(itemName, quantity);
+        products.add(new Product(itemName, quantity));
+        saveInventory();
     }
 
     public void updateQuantity(String itemName, Integer additionalQuantity) {
-        if (this.inventory.containsKey(itemName)) {
-            Integer currentQuantity = this.inventory.get(itemName);
-            this.inventory.put(itemName, currentQuantity + additionalQuantity);
-        } else {
-            this.inventory.put(itemName, additionalQuantity);
+        for (Product product : products) {
+            if (product.getName().equals(itemName)) {
+                int currentQuantity = product.getQuantity();
+                product.setQuantity(currentQuantity + additionalQuantity);
+                saveInventory();
+                return;
+            }
         }
+        // If item doesn't exist, add it
+        addItem(itemName, additionalQuantity);
     }
 
     public Integer getQuantity(String itemName) {
-        if (!this.inventory.containsKey(itemName))
-            return -1;
-        else
-            return this.inventory.get(itemName);
+         for (Product product : products) {
+            if (product.getName().equals(itemName)) {
+                return product.getQuantity();
+            }
+        }
+        return -1;
     }
+
+     // Save inventory to JSON file
+    private void saveInventory() {
+        try {
+            mapper.writeValue(new File(JSON_FILE), products);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+     // Get all products in inventory
+    public List<Product> getAllInventory() {
+        return products;
+    }
+    
     public ArrayList<Product> getAllInventory() {
         ArrayList<Product> allInventory = new ArrayList<Product>();
         Iterator<String> it = inventory.keySet().iterator();
@@ -45,6 +81,19 @@ public class Inventory {
             allInventory.add(product);
         }
         return allInventory;
+    }
+
+     // Load inventory from JSON file
+    private void loadInventory() {
+        try {
+            File file = new File(JSON_FILE);
+            if (file.exists()) {
+                CollectionType collectionType = mapper.getTypeFactory().constructCollectionType(List.class, Product.class);
+                products = mapper.readValue(file, collectionType);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
