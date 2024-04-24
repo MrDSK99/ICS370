59cd21722775915aa21f35ecff112bdd8dc71b99
Daniel Kouassi <143004381+MrDSK99@users.noreply.github.com>
2024-04-24 12:09:30 -0500

    Allow to registrer multiple users

--- Inventory_Application/src/main/java/com/metrosoftwaresolutions/inventory_application/Register_Controller.java
+++ Inventory_Application/src/main/java/com/metrosoftwaresolutions/inventory_application/Register_Controller.java
@@ -1,6 +1,5 @@
 package com.metrosoftwaresolutions.inventory_application;
 
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,15 +12,28 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.FileWriter;
 import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileReader;
+
 
 public class Register_Controller {
 
     private Stage stage;
     private Scene scene;
     private Parent root;
+
     @FXML
-    private Button closeButton;
+    private PasswordField passwordPasswordField;
+
+    @FXML
+    private PasswordField reenterPasswordField;
+
 
     @FXML
     private Button registerButton;
@@ -30,41 +42,60 @@ public class Register_Controller {
     private Label registrationMessageLabel;
 
     @FXML
-    private PasswordField passwordPasswordFiled;
+    private TextField usernameField;
 
-    @FXML
-    private PasswordField reenterPasswordFiled;
-
-    @FXML
-    private Label reenterPasswordLabel;
-
-    @FXML
-    private TextField usernameFiled;
 
     @FXML
     public void registerButtonOnAction(ActionEvent event) {
-        if (passwordPasswordFiled.getText().equals(reenterPasswordFiled.getText())) {
-            registerUser();
-            registrationMessageLabel.setText("Created new account successfully!");
-        }else {
-            reenterPasswordLabel.setText("Password does not match.");
+
+        String username = usernameField.getText();
+        String password = passwordPasswordField.getText();
+        String reenteredPassword = reenterPasswordField.getText();
+
+        if (!password.equals(reenteredPassword)) {
+            registrationMessageLabel.setText("Passwords do not match.");
+            return;
+        }
+
+        JSONArray userList = new JSONArray();
+
+        // Read existing user data from the file if it exists
+        try (FileReader reader = new FileReader("users.json")) {
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(reader);
+            if (obj instanceof JSONArray) {
+                userList = (JSONArray) obj;
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        // Create JSON object for the new user
+        JSONObject newUser = new JSONObject();
+        newUser.put("username", username);
+        newUser.put("password", password);
+
+        // Append the new user to the existing user list
+        userList.add(newUser);
+
+        // Write the updated user list back to the file
+        try (FileWriter file = new FileWriter("users.json")) {
+            file.write(userList.toJSONString());
+            registrationMessageLabel.setText("Registration successful.");
+        } catch (IOException e) {
+            registrationMessageLabel.setText("Error: Registration failed.");
+            e.printStackTrace();
         }
     }
 
-
-// need database for registerUser, also you can modifier registerButtonOnAction if needed
     @FXML
-    public void registerUser(){
-
-    }
-
-    @FXML
-    private void backButton(ActionEvent event) throws IOException {
-        //this code can be put in a method getScene(scene.fxml), used 3 times
-        root = FXMLLoader.load(getClass().getResource("Login_View.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+    void backButton(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Login_View.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
     }
 }
+
+
